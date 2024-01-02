96e983a42225ebf5be00d947e200e4d53e934cf9
Nihaal Sahay <nihaalsahaysjs@gmail.com>
2024-01-02 10:17:20 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://youtube.com/6packprogrammer" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://www.instagram.com/nihaalsahay/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/NihaalSahay" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
